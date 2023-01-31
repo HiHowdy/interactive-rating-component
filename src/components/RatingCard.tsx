34910bbe241b5fa726d6f5128e46b4e3561ef9fd
Liam (Howdy) <56754672+HiHowdy@users.noreply.github.com>
2023-01-31 21:17:16 +0000

    Fix card gradient

--- src/components/RatingCard.tsx
+++ src/components/RatingCard.tsx
@@ -4,6 +4,8 @@ type RatingCardProps = {
   children: React.ReactNode;
 };
 
+// bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-yellow-700 via-violet-500 to-zinc-800
+
 const TRANSITION = {
   initial: { opacity: 0, scale: 0 },
   animate: { opacity: 1, scale: 1, transition: { delay: 0.1 } },
@@ -14,7 +16,7 @@ const RatingCard = ({ children }: RatingCardProps) => {
   return (
     <motion.div
       {...TRANSITION}
-      className="bg-gradient-to-b from-base-300 to-base-200 p-7 rounded-3xl shadow-sm flex flex-col gap-8 w-[24rem] lg:w-[26rem]"
+      className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-base-300 to-base-200 p-7 rounded-3xl shadow-sm flex flex-col gap-8 w-[24rem] lg:w-[26rem]"
     >
       {children}
     </motion.div>
