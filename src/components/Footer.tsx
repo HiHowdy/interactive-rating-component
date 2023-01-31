fb405046c346919c197ed4945c2e58891cebf233
Liam (Howdy) <56754672+HiHowdy@users.noreply.github.com>
2023-01-31 21:19:09 +0000

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="absolute bottom-6 left-0 w-full text-center">
+      Challenge by{" "}
+      <a
+        className="text-primary"
+        href="https://www.frontendmentor.io/?ref=challenge"
+      >
+        Frontend Mentor
+      </a>{" "}
+      by{" "}
+      <a className="text-primary" href="https://github.com/HiHowdy">
+        HiHowdy
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
