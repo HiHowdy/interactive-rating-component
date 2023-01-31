b5db37a3c1bf93a94a71682e4fbd5514d0c3f5ed
Liam (Howdy) <56754672+HiHowdy@users.noreply.github.com>
2023-01-31 21:19:05 +0000

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 import InteractRating from "./components/InteractRating";
 
 function App() {
   return (
     <div className="w-screen h-screen absolute top-0 left-0 flex items-center justify-center">
       <InteractRating />
+      <Footer />
     </div>
   );
 }
